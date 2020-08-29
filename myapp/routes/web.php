34284bf990c486b47524bf2f8b5c8056af89a07b
yuryofujita <dasman74@hotmail.com>
2020-08-29 19:50:16 +0900

    PHP/Laravel 12 ユーザー認証を実装する

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -16,8 +16,12 @@ Route::get('/', function () {
 });
 
 Route::group(['prefix' => 'admin'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');;
 
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
