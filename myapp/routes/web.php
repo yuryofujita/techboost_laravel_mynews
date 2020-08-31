b7e3e24b6cfc75a5aa73e0c2c2b78a9744d279b3
yuryofujita <dasman74@hotmail.com>
2020-08-31 14:44:11 +0900

    PHP/Laravel 17 編集履歴を実装しよう

--- myapp/routes/web.php
+++ myapp/routes/web.php
@@ -20,6 +20,9 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('news/create', 'Admin\NewsController@add');
     Route::post('news/create', 'Admin\NewsController@create');
     Route::get('news', 'Admin\NewsController@index');
+    Route::get('news/edit', 'Admin\NewsController@edit');
+    Route::post('news/edit', 'Admin\NewsController@update');
+    Route::get('news/delete', 'Admin\NewsController@delete');
 
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/create', 'Admin\ProfileController@add');
