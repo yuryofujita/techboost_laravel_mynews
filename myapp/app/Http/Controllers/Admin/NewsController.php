cd5a661706f01aec83190b8955cb9390f35a57e0
yuryofujita <dasman74@hotmail.com>
2020-09-04 06:35:49 +0900

    あいまい検索追加

--- myapp/app/Http/Controllers/Admin/NewsController.php
+++ myapp/app/Http/Controllers/Admin/NewsController.php
@@ -52,7 +52,9 @@ class NewsController extends Controller
         $cond_title = $request->cond_title;
         if ($cond_title != '') {
             // 検索されたら検索結果を取得する
-            $posts = News::where('title', $cond_title)->get();
+            //$posts = News::where('title', $cond_title)->get();
+            //あいまい検索
+            $posts = News::where('title', 'like' , '%' . $cond_title . '%')->get();
         } else {
             // それ以外はすべてのニュースを取得する
             $posts = News::all();
